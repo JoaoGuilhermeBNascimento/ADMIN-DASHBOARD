5748b399f7e904918707f73194b775080f6d1e4a
João Guilherme B. do Nascimento <joaoguilhermebn@hotmail.com>
2023-02-15 17:33:57 -0300

    Topbar ready

--- react-admin/src/scenes/global/Topbar.jsx
+++ react-admin/src/scenes/global/Topbar.jsx
@@ -1,3 +1,19 @@
+import { Box, IconButton, useTheme } from '@mui/system';
+import { useContext } from 'react';
+import { ColorModeContext, tokens } from '../../theme';
+import { InputBase } from '@mui/material';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchIcon from '@mui/icons-material/SearchIcon';
 export function Topbar() {
-  return <div>Sidebar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return <Box display='flex' justifyContent='space-between'>
+
+  </Box>;
 }
